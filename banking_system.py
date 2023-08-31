396d5d3fb69145e2d0607e13fdc584eb3f47c9a0
Enzo Nonato <enzononato10@gmail.com>
2023-08-31 06:30:47 -0300

    atualizando o sistema bancario e adicionando a função de adicionar clientes (ainda falta fazer um sistema bancário personalizado para cada cliente)

--- banking_system.py
+++ banking_system.py
@@ -1,3 +1,4 @@
+
 import os
 import time
 
@@ -7,63 +8,131 @@ os.system('cls')
 saldo = 0
 limite = 500
 extrato = ""
-numero_saques = 0
+limiteSaques = 3
+numeroSaques = 0
+clientes= {}
 
 
-menu = """
-========== MENU ===========
 
-[1] Depositar
-[2] Sacar
-[3] Extrato
-[0] Sair
+def menu():
+    return """
+    ========== MENU ===========
 
-"""
+    [1] Depositar
+    [2] Sacar
+    [3] Extrato
+    [4] Cadastrar Novo Usuário
+    [0] Sair
+
+    ===========================
+
+    """
+
+def funcao_deposito(saldo, valorDeposito, extrato,/):
+    if valorDeposito > 0:
+        saldo+=valorDeposito
+        extrato += '\nDEPÓSITO -> R$'+str(valorDeposito)
+        print("Depósito efetuado com Sucesso!!!")
+    else:
+        print("Valor de depósito inválido!")
+    time.sleep(1)
+
+    return saldo, extrato
+
+def funcao_saque(*,saldo, valorSaque, extrato, limite, limiteSaques, numeroSaques):
+    if valorSaque > saldo:
+        print("Você não pode sacar um valor maior que o seu saldo")
+
+    elif valorSaque > limite:
+        print("\nO valor do seu saque não pode exceder R$ 500,00\n")
+
+    elif valorSaque <= limite and valorSaque > 0 and numeroSaques < limiteSaques and valorSaque <= saldo:
+        saldo-=valorSaque
+        extrato += '\nSAQUE -> R$'+str(valorSaque)
+        numeroSaques += 1
+
+    elif numeroSaques >= limiteSaques:
+            print(f"Você já fez os seus {limiteSaques} saques diários")
+    else:
+        print("Valor Inválido!")
+    time.sleep(1)
+
+    return saldo, extrato, numeroSaques
 
 
+
+def mostrar_extrato(saldo,/,*,extrato):
+    print(extrato)
+    print(f'\nSALDO = {saldo}')
+
+
+def cadastrar_usuário():
+    nome = input("Informe seu nome: ")
+    os.system("cls")
+
+    data_nascimento = input("Informe sua data de nascimento: ")
+    os.system("cls")
+
+    cpf = input("Informe seu CPF: ")
+    os.system("cls")
+
+    if cpf in clientes.keys():
+        print("CPF já existente")
+    else:
+        logradouro = input("Informe seu logradouro: ")
+        os.system("cls")
+
+        numero = input("Informe o número da casa: ")
+        os.system("cls")
+
+        bairro = input("Informe seu bairro: ")
+        os.system("cls")
+
+        cidade = input("Informe sua cidade: ")
+        os.system("cls")
+
+        estado = input("Informe a sigla do estado: ")
+        os.system("cls")
+
+
+        clientes.update({cpf : {"nome":nome, "data de nascimento":data_nascimento, "logradouro":logradouro, "numero":numero, "bairro":bairro, "cidade":cidade, "estado":estado}})
+
+    print(clientes)
+    print(clientes.keys())
+    time.sleep(2)
 
 
 while True:
 
-    opcao = input(menu)
+    opcao = input(menu())
 
-    if opcao == '1':
-        os.system('cls')
-        valorDeposito = float(input("Informe o valor do depósito: "))
-        if valorDeposito > 0:
-            saldo+=valorDeposito
-            extrato += '\nDEPÓSITO -> R$'+str(valorDeposito)
-            print("Depósito efetuado com Sucesso!!!")
-        else:
-            print("Valor de depósito inválido!")
-
-    elif opcao == '2':
-        os.system('cls')
-        print(f'\nSALDO = {saldo}\n')
-        valorSaque = float(input("Informe o valor do saque: "))
-        if valorSaque > limite:
-            print("\nO valor do seu saque não pode exceder R$ 500,00\n")
-        elif valorSaque > saldo:
-            print("Você não pode sacar um valor maior que o seu saldo")
-        elif valorSaque <= limite and valorSaque > 0 and numero_saques < 3 and valorSaque < saldo:
-            saldo-=valorSaque
-            extrato += '\nSAQUE -> R$'+str(valorSaque)
-            numero_saques += 1
-        elif numero_saques >= 3:
-            print("Você já fez os seus 3 saques diários")
-        else:
-            print("Valor Inválido!")
+    match opcao:
+        case '1':
+            os.system('cls')
+            valorDeposito = float(input("Informe o valor do depósito: "))
+            saldo, extrato = funcao_deposito(saldo,valorDeposito,extrato)
         
+        case '2':
+            os.system('cls')
+            print(f'\nSALDO = {saldo}\n')
+            valorSaque = float(input("Informe o valor do saque: "))
+            saldo, extrato, numeroSaques = funcao_saque(saldo=saldo, valorSaque=valorSaque,extrato=extrato, limite=limite, limiteSaques=limiteSaques, numeroSaques=numeroSaques)
+            
 
-    elif opcao == '3':
-        os.system('cls')
-        print(extrato)
-        print(f'\nSALDO = {saldo}')
+        case '3':
+            os.system('cls')
+            mostrar_extrato(saldo,extrato=extrato)
+            time.sleep(1)
+        
+        case '4':
+            cadastrar_usuário()
 
-    elif opcao == '0':
-        break
-
-    else:
-        os.system('cls')
-        print('Opção inválida tente novamente...')    
-        time.sleep(1)
+        case '0':
+            print("Saindo...")
+            time.sleep(1)
+            break
+        
+        case _:
+            os.system('cls')
+            print('Opção inválida tente novamente...')    
+            time.sleep(1)
